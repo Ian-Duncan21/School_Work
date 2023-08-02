f63f90cebbfbe43188d00050148b205ca2432a82
Ian-Duncan21 <111916006+Ian-Duncan21@users.noreply.github.com>
2023-08-02 13:27:46 -0600

    Add files via upload

---
+++ CS253_Part1/hw0/testdir/main.cc
@@ -0,0 +1,8 @@
+        #include <iostream>
+        using namespace std;
+
+        int main() {
+            // How many psychologists does it take to change a light bulb?
+            cout << "One but the lightbulb really has to want to change \n";
+            return 0;
+        }
